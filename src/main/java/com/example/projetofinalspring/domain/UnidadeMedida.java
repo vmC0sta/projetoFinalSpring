8ce8d2da8c0c3312e87471e8976436619dfbaaf3
ViniciusMathS <viniciusmsms@gmail.com>
2023-12-11 11:41:53 -0300

    Criando entidades
    Foi criado a entidade UnidadeMedida

---
+++ src/main/java/com/example/projetofinalspring/domain/UnidadeMedida.java
@@ -0,0 +1,45 @@
+package com.example.projetofinalspring.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "unidademedida")
+public class UnidadeMedida {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(length = 45)
+	private String descricao;
+	@Column(length = 45)
+	private String sigla;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+}
