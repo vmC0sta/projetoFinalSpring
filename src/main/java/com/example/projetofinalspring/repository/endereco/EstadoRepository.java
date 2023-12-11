f3e61cd627f36941925a374becf342e0b607f193
vmC0sta <duda.cv2404@gmail.com>
2023-12-11 14:41:57 -0300

    Adicionado EstadoRepository

---
+++ src/main/java/com/example/projetofinalspring/repository/endereco/EstadoRepository.java
@@ -0,0 +1,7 @@
+package com.example.projetofinalspring.repository.endereco;
+
+import com.example.projetofinalspring.domain.endereco.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepository extends JpaRepository<Estado,Long> {
+}
