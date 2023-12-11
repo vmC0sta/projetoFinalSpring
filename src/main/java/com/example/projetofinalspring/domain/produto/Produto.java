70628b410bc61b68463a80c54c954449665664fc
ViniciusMathS <viniciusmsms@gmail.com>
2023-12-11 14:43:56 -0300

    Criando entidades
    Foi criado as entidades Estoque e Produto

---
+++ src/main/java/com/example/projetofinalspring/domain/produto/Produto.java
@@ -0,0 +1,67 @@
+package com.example.projetofinalspring.domain.produto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(length = 45)
+	private String descricao;
+	@Column(length = 45)
+	private String codigoReferencia;
+	@ManyToOne 
+	private Categoria categoria;
+	@ManyToOne 
+	private UnidadeMedida unidadeMedida;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCodigoReferencia() {
+		return codigoReferencia;
+	}
+
+	public void setCodigoReferencia(String codigoReferencia) {
+		this.codigoReferencia = codigoReferencia;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public UnidadeMedida getUnidadeMedida() {
+		return unidadeMedida;
+	}
+
+	public void setUnidadeMedida(UnidadeMedida unidadeMedida) {
+		this.unidadeMedida = unidadeMedida;
+	}
+	
+}
