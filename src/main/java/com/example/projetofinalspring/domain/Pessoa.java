441fc702e975903f4942bf209f6f4f7218cd0844
vmC0sta <duda.cv2404@gmail.com>
2023-12-11 11:05:01 -0300

    commit pasta endereco

--- src/main/java/com/example/projetofinalspring/domain/Pessoa.java
+++
@@ -1,4 +0,0 @@
-package main.java.com.example.projetofinalspring.domain;
-
-public class Pessoa {
-}
--- src/main/java/com/example/projetofinalspring/repository/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projetofinalspring.repository;
-import main.java.com.example.projetofinalspring.domain.Pessoa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
